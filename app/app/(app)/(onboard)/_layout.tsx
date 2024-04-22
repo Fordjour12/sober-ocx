db4e3d49957975b2e3283ffbef183e19d227ed00
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-04-22 14:00:30 +0000

    Onboarding question check
    route to the respective section of the onboarding question flow

--- app/app/(app)/(onboard)/_layout.tsx
+++ app/app/(app)/(onboard)/_layout.tsx
@@ -1,8 +1,30 @@
 import { getStoreValue } from "@/hooks/secureStore.hooks";
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
 import React from "react";
 
 export default function OnBoardLayout() {
+
+	React.useEffect(() => {
+
+		const checkOnboarding = async () => {
+			const onboardingId = await getStoreValue("OnBoardingID")
+			const reasonAdded = await getStoreValue("reasonAdded")
+
+			if (onboardingId && reasonAdded) {
+				router.push("/register")
+			} else if (!onboardingId) {
+				router.push("/sober-date")
+			} else if (!reasonAdded) {
+				router.push("/reason")
+			}
+		}
+
+		checkOnboarding()
+
+	}, []);
+
+
+
 	return (
 		<Stack
 			screenOptions={{
