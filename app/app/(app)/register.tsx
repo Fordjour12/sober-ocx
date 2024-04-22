1658f37cceefdec86acd8bf01a0f1e7e4b971f70
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-04-22 14:01:22 +0000

    Register User
    the flow for registering user and

--- app/app/(app)/register.tsx
+++ app/app/(app)/register.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/Button";
 import { QuickSandBold, QuickSandRegular } from "@/components/StyledText";
 import TextInputWithLabel from "@/components/TextInputWithLabel";
+import { getStoreValue } from "@/hooks/secureStore.hooks";
+import axios from "axios";
 import React, { useState } from "react";
 import {
 	Keyboard,
@@ -11,10 +13,54 @@ import {
 	View,
 } from "react-native";
 
+
+type RegisterUserProps = {
+	username: string;
+	password: string;
+	email: string;
+}
+
 export default function register() {
 	const [username, setUsername] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
 	const [email, setEmail] = useState<string>("");
+
+
+	const createNewUser = async ({ username, password, email }: RegisterUserProps) => {
+
+		const onboardingId = JSON.parse(String(await getStoreValue("OnBoardingID")))
+
+		try {
+			const response = await axios.post(
+				"http://192.168.104.242:3000/auth/register",
+				{
+					username,
+					password,
+					email
+				},
+				{
+					headers: {
+						"Content-Type": "multipart/form-data"
+					}
+				})
+
+			if (response.status === 201) {
+				console.log(response.data)
+				await axios.put(
+					`http://192.168.104.242:3000/${onboardingId}`, {
+					userId: response.data.created[0].id
+				}, {
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+			}
+
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
 
 	return (
 		<KeyboardAvoidingView
@@ -54,7 +100,7 @@ export default function register() {
 						<Button
 							style={styles.btn}
 							title="Create Account"
-							onPress={() => {}}
+							onPress={() => createNewUser({ username, password, email })}
 						/>
 					</View>
 				</>
