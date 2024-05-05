856b1728a0cb435b871ea597a6942ad29da1af39
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-05 07:36:54 +0000

    conselors mock

--- app/app/(app)/(root)/counseling.tsx
+++ app/app/(app)/(root)/counseling.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View } from "@/components/Themed";
+import Counselor from "@/constants/conselorsData.json"
+import { ScrollView } from "react-native";
+import { CircleUserRoundIcon } from "lucide-react-native";
+import { QuickSandMedium, QuickSandRegular, QuicksandSemiBold } from "@/components/StyledText";
 
 export default function counseling() {
 	return (
-		<View>
-			<Text>counseling</Text>
-		</View>
+		<ScrollView>
+			<QuickSandMedium>The best counselors we have </QuickSandMedium>
+
+			{
+				Counselor.map((item, index) => {
+					return (
+						<View key={index}
+							style={{ backgroundColor: "red" }}
+							className="p-4 m-3 flex flex-row items-center gap-6 rounded">
+
+							< CircleUserRoundIcon color={"#fff"} size={50} />
+							<View style={{ backgroundColor: "transparent" }}>
+								<QuicksandSemiBold className="text-xl py-2" >{item.name}</QuicksandSemiBold>
+								<QuickSandRegular>{item.area_of_expertise}</QuickSandRegular>
+							</View>
+						</View>
+					)
+				})
+			}
+
+
+		</ScrollView>
 	);
 }
