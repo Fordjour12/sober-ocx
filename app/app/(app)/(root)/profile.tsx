35e2bbb006ab2096f871c0db4a39f273f7a8a20b
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-05 07:37:57 +0000

    profile ui
    delete and logout buttons

--- app/app/(app)/(root)/profile.tsx
+++ app/app/(app)/(root)/profile.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View } from "@/components/Themed";
+import { TouchableOpacity } from "react-native";
+import { QuickSandMedium } from "@/components/StyledText";
+
+
 
 export default function profile() {
 	return (
 		<View>
-			<Text>profile</Text>
+
+			<View>
+			</View>
+
+			<View>
+
+			</View>
+
+			<View className="my-4">
+
+				<TouchableOpacity style={{ backgroundColor: "gray" }} className="p-3 mx-3 my-4 rounded">
+					<QuickSandMedium className="text-center text-lg"> Sign Out </QuickSandMedium>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={{ backgroundColor: "red" }} className="p-3 mx-3 rounded">
+					<QuickSandMedium className="text-center text-lg"> Delete Account </QuickSandMedium>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
